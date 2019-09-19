c2e6585233f7d9d5c55586803237cb16c6e90302
danylo.goncharov <danylo.goncharov@xing.com>
2019-09-19 10:57:23 +0200

    arg parse

--- script.py
+++ script.py
@@ -1,4 +1,29 @@
+import argparse
+import os.path
+import yaml
+
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c', '--config', dest='config', action='store', type=str,
+        help='path to custom config',
+        default=os.path.join(os.path.dirname(__file__), "config.yaml")
+    )
+    return parser
+
+
+def load_config(file_name):
+    with open(file_name,"r") as conf_file:
+        return yaml.safe_load(conf_file.read())
+
+
 def main():
-	print("Hello world")
-if(__name__=="__main__"):
-	main()
+    parser = build_parser()
+    params, other_params = parser.parse_known_args()
+    conf = load_config(params.config)
+    print(conf)
+
+
+if __name__ == "__main__":
+    main()
